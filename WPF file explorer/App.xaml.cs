f88fa5183a36f8b71d8358d455bcefba5c311819
KacperKotlewski <kotlewski.kacper@gmail.com>
2018-11-19 08:34:43 +0100

    Add project files.

---
+++ WPF file explorer/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_file_explorer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
